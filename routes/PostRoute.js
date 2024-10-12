9269419dd2185b46a4f61f3394320059839c7dee
inno-cent <ekweozorinnocent@gmail.com>
2024-10-12 03:22:25 +0100

    feat : controller update

--- routes/PostRoute.js
+++ routes/PostRoute.js
@@ -1,11 +1,14 @@
 const express = require('express')
-const {newBlog,getBlogs, getBlogById, updateBlog} = require('../controller/Post')
+const {newBlog,getBlogs, getBlogById, updateBlog,deletePost} = require('../controller/Post')
 
 const router = express.Router();
 
 router.post('/', newBlog)
 router.get('/', getBlogs)
 router.get('/:id', getBlogById)
+router.put('/:id', updateBlog)
+router.delete('/:id', deletePost)
+
 
 
 module.exports = router
