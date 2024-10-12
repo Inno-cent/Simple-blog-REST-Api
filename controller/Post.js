b6bde2cd4e02ab16b25d53a518bb3c9e5c2859ff
inno-cent <ekweozorinnocent@gmail.com>
2024-10-12 03:21:33 +0100

    feat : controller update

--- controller/Post.js
+++ controller/Post.js
@@ -12,11 +12,50 @@ exports.newBlog = async (req, res) => {
   }
 };
 
+// Updating all post
 exports.getBlogs = async (req, res) => {
   try {
-    const posts = await POST.find();
-    res.status(201).json(posts);
+    const blogs = await POST.find();
+    res.status(201).json(blogs);
   } catch (err) {
     console.error("Error message :", err);
+  }
+};
+
+// getting a post by ID controller
+exports.getBlogById = async (req, res) => {
+  try {
+    const Blog = await POST.findById(req.params.id);
+    res.status(201).json(Blog);
+  } catch (err) {
+    console.error("Error message :", err);
+  }
+};
+
+// Updating a post by ID controller
+exports.updateBlog = async (req, res) => {
+  try {
+    const { title, content, author } = req.body;
+    const updatedPost = await Post.findByIdAndUpdate(
+      req.params.id,
+      { title, content, author },
+      { new: true }
+    );
+    if (!updatedPost)
+      return res.status(404).json({ message: "Post not found" });
+    res.json(updatedPost);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.deletePost = async (req, res) => {
+  try {
+    const deletedPost = POST.findByIdAndDelete(req.params.id);
+    if (!deletedPost)
+      return res.status(401).json({ message: "Post not found" });
+    res.json(deletedPost);
+  } catch (error) {
+    res.status(500).json({ message: err.message });
   }
 };
--- routes/PostRoute.js
+++ routes/PostRoute.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const {newBlog,getBlogs} = require('../controller/Post')
+const {newBlog,getBlogs, getBlogById, updateBlog} = require('../controller/Post')
 
 const router = express.Router();
 
 router.post('/', newBlog)
 router.get('/', getBlogs)
+router.get('/:id', getBlogById)
 
 
 module.exports = router
